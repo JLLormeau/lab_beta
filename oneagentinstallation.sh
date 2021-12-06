6db1ca62b535e9c8ca516678520ca538867d54e3
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2021-12-06 12:31:39 +0100

    Delete oneagentinstallation.sh

--- oneagentinstallation.sh
+++
@@ -1,2 +0,0 @@
-wget  -O Dynatrace-OneAgent-Linux-latest.sh "https://$MyTenant/api/v1/deployment/installer/agent/unix/default/latest?arch=x86&flavor=default" --header="Authorization: Api-Token $PaasToken"
-sudo /bin/sh Dynatrace-OneAgent-Linux-lates.sh --set-host-group=$Appname --set-host-property=env=sandbox
